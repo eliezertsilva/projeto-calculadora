b35bdd6df80109a9034b22fcbf672d346206ac0a
Eliezer Tonkelski da Silva <elitonkelski@gmail.com>
2022-03-04 23:57:16 -0400

    estrutura-js-pt1

---
+++ script.js
@@ -0,0 +1,15 @@
+var num = document.querySelector('.num');
+var del = document.querySelector('#del');
+var calc = document.querySelector('#calc');
+var func = document.querySelector('.func');
+var resultado = document.querySelector('#resultado');
+
+function num(){
+    let teste = 1+1;
+    resultado.innerHTML = teste;
+        /*switch (num) {
+        case 0:
+            resultado.innerHTML = "teste";
+            break;
+    }*/
+}
