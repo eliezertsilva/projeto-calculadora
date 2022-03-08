02b195472cc9b757e51db4f6b4637be534e1ebc8
Eliezer Tonkelski da Silva <elitonkelski@gmail.com>
2022-03-08 11:32:23 -0400

    att

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-var num = document.querySelector('.num');
-var del = document.querySelector('#del');
-var calc = document.querySelector('#calc');
-var func = document.querySelector('.func');
-var resultado = document.querySelector('#resultado');
+resultado = document.querySelector('p')
 
 function num(){
-    let teste = 1+1;
-    resultado.innerHTML = teste;
-        /*switch (num) {
-        case 0:
-            resultado.innerHTML = "teste";
-            break;
-    }*/
+    resultado.innerHTML = ' 8 ';
 }
